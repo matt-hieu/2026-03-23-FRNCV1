2d3b2ea6b7bf3703ecd38244399c270a4e340cec
champix56 <desorbaix@free.fr>
2026-03-23 15:26:06 +0100

    usage de children

--- App.tsx
+++ App.tsx
@@ -6,10 +6,7 @@ import Button from './src/Button/Button';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Button text="blabla">
-        <View><Text>Hello</Text></View>
-        <View><Text>De</Text></View>
-      </Button>
+      <Button text="blabla">Coucou</Button>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
