68f6ced2aead9445275046a7e517a7056e880913
champix56 <desorbaix@free.fr>
2026-03-23 16:56:48 +0100

    deestructuration de props avec default values

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -1,14 +1,14 @@
 import { View, Text, Pressable,ToastAndroid } from "react-native";
 import styles from "./Button.styles";
-const Button = (props) => {
-  console.log(props)
+const Button = ({children, color, bgColor, onButtonPressed=()=>{}}) => {
+  //console.log(props)
   return (
     <Pressable onPress={(evt)=>{
         //console.log(evt);
         ToastAndroid.show("pressed", ToastAndroid.LONG);
-        props.onButtonPressed()
-    }} style={[styles.Button, {backgroundColor:props.bgColor}]}>
-      <Text style={[styles.text,{color:props.color}]}>{props.children}</Text>
+        onButtonPressed()
+    }} style={[styles.Button, {backgroundColor:bgColor}]}>
+      <Text style={[styles.text,{color:color}]}>{children}</Text>
     </Pressable>
   );
 };
