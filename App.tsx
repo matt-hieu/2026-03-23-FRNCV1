83f423940b4da2b57f3241f9e730d6e9aed0df4f
champix56 <desorbaix@free.fr>
2026-03-23 17:42:21 +0100

    useEffect

--- App.tsx
+++ App.tsx
@@ -1,10 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import Button from './src/Button/Button';
 
 export default function App() {
   const [counter,setCounter]=useState(0)
+  useEffect(()=>{
+    console.log('updated:',counter);
+  },[counter])
+  useEffect(()=>{
+    console.log('montage du composant');
+    return ()=>{
+      console.log('demontage du composant')
+    }
+  },[])
   return (
     <View style={styles.container}>
       <Text>valeur de counter : {counter}</Text>
