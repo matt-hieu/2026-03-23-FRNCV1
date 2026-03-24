5a76f9b8ce768fedde7891d58932d98314dfee03
champix56 <desorbaix@free.fr>
2026-03-24 17:37:49 +0100

    update search value

--- src/store/productsSlice.ts
+++ src/store/productsSlice.ts
@@ -1,31 +1,41 @@
-import { createSlice } from '@reduxjs/toolkit'
-import { IProduct } from '../interfaces/IProducts';
-interface IProductsSliceState{
-    products:Array<IProduct>
-    selectedProduct:undefined|IProduct
-    filtredProducts:Array<IProduct>
-    search:string
+import { createSlice } from "@reduxjs/toolkit";
+import { IProduct } from "../interfaces/IProducts";
+interface IProductsSliceState {
+  products: Array<IProduct>;
+  selectedProduct: undefined | IProduct;
+  filtredProducts: Array<IProduct>;
+  search: string;
 }
-const initialState:IProductsSliceState = {
-    products:[],
-    filtredProducts:[],
-    selectedProduct:undefined,
-    search:''
-}
+const initialState: IProductsSliceState = {
+  products: [],
+  filtredProducts: [],
+  selectedProduct: undefined,
+  search: "",
+};
 
 const productsSlice = createSlice({
-  name: 'products',
+  name: "products",
   initialState,
   reducers: {
-   initialProductLoad:(state,action:{type:string,payload:Array<IProduct>})=>{
-    //vidange
-    state.products.splice(0)
-    //remplissage avec chaque element contenu dans l'array un par un 
-    state.products.push(...action.payload)
-   }
-  }
+    updateSearch: (state, action: { type: string; payload: string }) => {
+      state.search = action.payload;
+      state.filtredProducts = state.products.filter((p) =>
+        p.titre.toLowerCase().includes(action.payload.toLowerCase()),
+      );
+    },
+    initialProductLoad: (
+      state,
+      action: { type: string; payload: Array<IProduct> },
+    ) => {
+      //vidange
+      state.products.splice(0);
+      //remplissage avec chaque element contenu dans l'array un par un
+      state.products.push(...action.payload);
+      state.filtredProducts = state.products;
+    },
+  },
 });
 
-export const {initialProductLoad} = productsSlice.actions
-const productsReducer= productsSlice.reducer
-export default productsReducer
+export const { initialProductLoad, updateSearch } = productsSlice.actions;
+const productsReducer = productsSlice.reducer;
+export default productsReducer;
