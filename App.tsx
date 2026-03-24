a7d4955652385e5d8dbc209ca2728b0ff76ba81c
champix56 <desorbaix@free.fr>
2026-03-24 10:23:38 +0100

    clear project for fetch access

--- App.tsx
+++ App.tsx
@@ -1,31 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import React, {useEffect, useState} from 'react'
-import Button from './src/Button/Button';
+
 
 export default function App() {
-  const [counter,setCounter]=useState(0)
-  useEffect(()=>{
-    console.log('updated:',counter);
-  },[counter])
-  useEffect(()=>{
-    console.log('montage du composant');
-    return ()=>{
-      console.log('demontage du composant')
-    }
-  },[])
+
   return (
     <View style={styles.container}>
-      <Text>valeur de counter : {counter}</Text>
-      <Button bgColor="red" color="white" onButtonPressed={()=>{
-        setCounter(counter-1)
-        console.log(counter)
-      }} >-1</Button>
-      <Button bgColor="blue" color="white" onButtonPressed={()=>{
-        setCounter(counter+1)
-        console.log(counter);
-      }}>+1</Button>
-      <StatusBar style="auto" />
+      <Text></Text>
     </View>
   );
 }
