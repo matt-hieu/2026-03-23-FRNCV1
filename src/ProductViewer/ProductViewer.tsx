f5bbdf5e62150a24bbb992f709004941f93ae953
champix56 <desorbaix@free.fr>
2026-03-24 12:32:44 +0100

    base style

--- src/ProductViewer/ProductViewer.tsx
+++ src/ProductViewer/ProductViewer.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { IProduct } from "../interfaces/IProducts";
 
@@ -9,15 +9,17 @@ interface IProductViewerProps {
 const ProductViewer = ({ product }: IProductViewerProps) => {
   return (
     <View style={styles.ProductViewer}>
-      <Text style={styles.titre}>ProductViewer</Text>
+      <Text style={[styles.titre,styles.bold]}>{product.titre}</Text>
       <View style={styles.colsFlex}>
         <View style={styles.colLeft}>
-            <Text style={styles.stock}>stock:</Text>
-            <Text style={styles.descriptionLabel}>Description :</Text>
-            <Text style={styles.description}></Text>
-            <Text style={styles.prix}>Prix:</Text>
+          <Text style={styles.stock}>stock:{product.stock}</Text>
+          <Text style={styles.descriptionLabel}>Description :</Text>
+          <Text style={styles.description}>{product.description}</Text>
+          <Text style={styles.prix}>Prix:{product.prix}</Text>
         </View>
-        <View style={styles.colRight}></View>
+        <View style={styles.colRight}>
+          <Button title="Edition" />
+        </View>
       </View>
     </View>
   );
@@ -26,9 +28,29 @@ const ProductViewer = ({ product }: IProductViewerProps) => {
 export default ProductViewer;
 
 const styles = StyleSheet.create({
-  ProductViewer: {},
-  titre: {},
-  colsFlex: {},
+  ProductViewer: {
+    borderColor: "grey",
+    borderWidth: 1,
+    borderStyle: "solid",
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+  },
+  titre: {
+    textAlign: "center",
+    fontSize: 20,
+    textDecorationLine: "underline",
+  },
+  colsFlex: {
+    flexDirection:'row',
+    gap:5
+  },
   colLeft: {},
-  colRight: {},
+  colRight: {
+    justifyContent:'center'
+  },
+  stock: {},
+  descriptionLabel: {},
+  description: {},
+  prix: {},
+  bold:{fontWeight:900}
 });
