d44e69f0e67d9491af4db85784d6bd7f71d48810
champix56 <desorbaix@free.fr>
2026-03-24 13:02:21 +0100

    add scrollview

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Dimensions, ScrollView, StyleSheet, Text, View } from "react-native";
 import React, { useEffect, useState } from "react";
 import ProductViewer from "./src/ProductViewer/ProductViewer";
 import { IProduct } from "./src/interfaces/IProducts";
@@ -8,11 +8,7 @@ export default function App() {
   const [products, setProducts] = useState<Array<IProduct>>([]);
   useEffect(() => {
     fetch(
-      `${
-        process.env.EXPO_PUBLIC_API_URL
-      }:${
-        process.env.EXPO_PUBLIC_API_PORT
-      }${
+      `${process.env.EXPO_PUBLIC_API_URL}:${process.env.EXPO_PUBLIC_API_PORT}${
         process.env.EXPO_PUBLIC_API_ENDPOINT_PRODUCTS
       }`,
     )
@@ -21,11 +17,15 @@ export default function App() {
   }, []);
 
   return (
-    <View style={styles.container}>
-      {products.map((element,index)=>{
-          return <ProductViewer key={element.id} product={element}/>
-        })}
-      {/*<Text>{JSON.stringify(products)}</Text>*/}
+    <View style={{ flex: 1 }}>
+      <ScrollView>
+        <View style={styles.container}>
+          {products.map((element, index) => {
+            return <ProductViewer key={element.id} product={element} />;
+          })}
+          {/*<Text>{JSON.stringify(products)}</Text>*/}
+        </View>
+      </ScrollView>
     </View>
   );
 }
@@ -34,7 +34,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    // alignItems: "center",
+    // justifyContent: "center",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-between",
   },
 });
