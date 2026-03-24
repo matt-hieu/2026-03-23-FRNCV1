09bf05e6f8aa4eada09ef8dca6c55f1507301a3f
champix56 <desorbaix@free.fr>
2026-03-24 17:38:16 +0100

    connexion de fonction de dispatch

--- src/components/functionnals/ProductsSearcher/ProductsSearcher.connected.tsx
+++ src/components/functionnals/ProductsSearcher/ProductsSearcher.connected.tsx
@@ -1,28 +1,42 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import ProductsSearcherUnconnected from './ProductsSearcher'
-import { connect, useSelector } from 'react-redux'
+import { View, Text } from "react-native";
+import React from "react";
+import ProductsSearcherUnconnected from "./ProductsSearcher";
+import { connect, useDispatch, useSelector } from "react-redux";
+import { updateSearch } from "../../../store/productsSlice";
 type Props = {
-    onSearchChange:(search:string)=>void
-}
+  // onSearchChange:(search:string)=>void
+};
 
 const ProductsSearcher = (props: Props) => {
-    const products=useSelector((s:any)=>s.stock.products)
-    const search=useSelector((s:any)=>s.stock.search)
-
+  const products = useSelector((s: any) => s.stock.products);
+  const search = useSelector((s: any) => s.stock.search);
+  const dispatch = useDispatch();
   return (
-    <ProductsSearcherUnconnected {...props} products={products} searchField={search}/>
-  )
-}
+    <ProductsSearcherUnconnected
+      {...props}
+      onSearchChange={(searchFieldVal) => {
+        dispatch(updateSearch(searchFieldVal));
+      }}
+      products={products}
+      searchField={search}
+    />
+  );
+};
 
-export default ProductsSearcher
+export default ProductsSearcher;
 
-function mapDispatchToProps(dispatch:Function){return{}}
-function mapStateToProps(props:Props,state:any){
-    return {
-        ...props,
-        products:state.stock.products,
-        search:state.stock.search
-    }
+function mapDispatchToProps(dispatch: Function) {
+  return {
+    onSearchChange:(str)=>dispatch(updateSearch(str))
+  };
 }
-export const ProductsSearcherNoHook=connect(mapStateToProps)(ProductsSearcherUnconnected)
+function mapStateToProps(props: Props, state: any) {
+  return {
+    ...props,
+    products: state.stock.products,
+    search: state.stock.search,
+  };
+}
+export const ProductsSearcherNoHook = connect(mapStateToProps, mapDispatchToProps)(
+  ProductsSearcherUnconnected,
+);
