7e88a7e7daf69db1d1b7fa02cc535e987782c223
champix56 <desorbaix@free.fr>
2026-03-24 12:11:17 +0100

    structure de base du product viewer

--- src/ProductViewer/ProductViewer.tsx
+++ src/ProductViewer/ProductViewer.tsx
@@ -1,18 +1,34 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import {IProduct} from '../interfaces/IProducts'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { IProduct } from "../interfaces/IProducts";
+
 interface IProductViewerProps {
-    product:IProduct
+  product: IProduct;
 }
 
-const ProductViewer = ({product}: IProductViewerProps) => {
+const ProductViewer = ({ product }: IProductViewerProps) => {
   return (
-    <View>
-      <Text></Text>
+    <View style={styles.ProductViewer}>
+      <Text style={styles.titre}>ProductViewer</Text>
+      <View style={styles.colsFlex}>
+        <View style={styles.colLeft}>
+            <Text style={styles.stock}>stock:</Text>
+            <Text style={styles.descriptionLabel}>Description :</Text>
+            <Text style={styles.description}></Text>
+            <Text style={styles.prix}>Prix:</Text>
+        </View>
+        <View style={styles.colRight}></View>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default ProductViewer
+export default ProductViewer;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  ProductViewer: {},
+  titre: {},
+  colsFlex: {},
+  colLeft: {},
+  colRight: {},
+});
