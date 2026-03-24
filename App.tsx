479591bb2fd45bb17fb072fde498c32f47ad1493
champix56 <desorbaix@free.fr>
2026-03-24 10:41:26 +0100

    fetch de data sur composant

--- App.tsx
+++ App.tsx
@@ -1,13 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import React, {useEffect, useState} from 'react'
-
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
 
 export default function App() {
+  const [products, setProducts] = useState<Array<object>>([]);
+  useEffect(() => {
+    fetch(
+      `${
+        process.env.EXPO_PUBLIC_API_URL
+      }:${
+        process.env.EXPO_PUBLIC_API_PORT
+      }${
+        process.env.EXPO_PUBLIC_API_ENDPOINT_PRODUCTS
+      }`,
+    )
+      .then((r) => r.json())
+      .then((a) => setProducts(a));
+  }, []);
 
   return (
     <View style={styles.container}>
-      <Text></Text>
+      <Text>{JSON.stringify(products)}</Text>
     </View>
   );
 }
@@ -15,8 +28,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
