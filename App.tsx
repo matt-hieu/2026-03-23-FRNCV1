353b9a935ad4614e2e184416debd1a9c1da3ee5e
champix56 <desorbaix@free.fr>
2026-03-24 17:28:08 +0100

    provider + composants connecté en valeurs

--- App.tsx
+++ App.tsx
@@ -2,25 +2,23 @@ import { ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
 import React, { useEffect, useState } from "react";
 import { IProduct } from "./src/interfaces/IProducts";
 import ProductsListViewer from "./src/components/ui/ProductsListViewer/ProductsListViewer.connected";
-import ProductsSearcher from "./src/components/functionnals/ProductsSearcher/ProductsSearcher";
 import Banner from "./src/components/ui/Banner/Banner";
 import {store} from './src/store/store'
 import { Provider } from "react-redux";
+import ProductsSearcher from "./src/components/functionnals/ProductsSearcher/ProductsSearcher.connected";
 export default function App() {
   console.log(store);
   const [search, setSearch] = useState("");
-  const [selectedProduct, setselectedProduct] = useState<undefined|IProduct>(undefined)
+  //const [selectedProduct, setselectedProduct] = useState<undefined|IProduct>(undefined)
   return (
     <Provider store={store}>
       <View style={{ flex: 1, }}>
         <Banner/>
-        {/* <ProductsSearcher
-          products={products}
-          searchField={search}
+        <ProductsSearcher
           onSearchChange={(str) => {
             setSearch(str);
           }}
-        /> */}
+        />
         <ScrollView>
           <ProductsListViewer/>
         </ScrollView>
